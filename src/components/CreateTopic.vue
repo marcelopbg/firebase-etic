7e87fca9e41f345560b15b48d1e07c4c89b17782
marcelopbg <mpbguima@gmail.com>
2019-09-26 22:23:22 -0300

    static image

--- src/components/CreateTopic.vue
+++ src/components/CreateTopic.vue
@@ -1,66 +1,79 @@
 <template>
-    <v-container>
+  <v-container>
     <h3 class="indigo--text text--darken-2">Criar discussão</h3>
-  <v-form>
-    <v-container>
-      <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="title"
-            :counter="50"
-            :rules="titleRules"
-            label="Título"
-            required
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-           <v-textarea
-           v-model="description"
-           :rules="descriptionRules"
-           :counter="200"
-          name="input-7-1"
-          label="Descrição"
-        ></v-textarea>
-        </v-col>
-        <v-btn class="ma-2" tile color="indigo" dark v-on:click="sendShitToFb()"> Salvar </v-btn>
-      </v-row>
-    </v-container>
-  </v-form>
+    <v-form>
+      <v-container class="justify-center">
+        <v-row>
+          <v-text-field v-model="title" :counter="50" :rules="titleRules" label="Título" required></v-text-field>
+        </v-row>
+        <v-row>
+          <v-textarea
+            v-model="description"
+            :rules="descriptionRules"
+            :counter="200"
+            name="input-7-1"
+            label="Descrição"
+          ></v-textarea>
+        </v-row>
+         <input
+            class="form-control"
+            type="file"
+            ref="fileInput"
+            style
+            accept="image/*"
+            @change="onFilePicked"
+          >
+        <v-row>
+          <v-btn class="ma-2" tile color="indigo" dark v-on:click="sendShitToFb()">Salvar</v-btn>
+        </v-row>
+      </v-container>
+    </v-form>
   </v-container>
 </template>
 
 <script>
-import firebase from 'firebase';
+import firebase from "firebase";
 
 export default {
   data: () => ({
-      title: "",
-          titleRules: [
-        v => !!v || 'Título deve ser preenchido',
-        v => (v && v.length <= 50) || 'Título deve conter no máximo 50 caractéres',
-      ],
-      description: "",
-      descriptionRules: [
-        v => !!v || 'Descrição deve ser preenchida',
-        v => (v && v.length <= 200) || 'Descrição deve conter no máximo 200 caractéres',
-      ],
+    title: "",
+    image: "",
+    fileName: "",
+    titleRules: [
+      v => !!v || "Título deve ser preenchido",
+      v => (v && v.length <= 50) || "Título deve conter no máximo 50 caractéres"
+    ],
+    description: "",
+    descriptionRules: [
+      v => !!v || "Descrição deve ser preenchida",
+      v =>
+        (v && v.length <= 200) ||
+        "Descrição deve conter no máximo 200 caractéres"
+    ]
   }),
   methods: {
-      sendShitToFb() {         
-          try {
-          const response = firebase.database().ref('topicos/').push({title: this.title, description: this.description})
-          alert(response);
-        } catch(e) {
-            return e;
-        }
+    sendShitToFb() {
+      try {
+        const response = firebase
+          .database()
+          .ref("topicos/")
+          .push({ title: this.title, description: this.description, timestamp: new Date().getTime() });
+        alert(response);
+      } catch (e) {
+        return e;
       }
+    },
+      onFilePicked(event) {
+      const files = event.target.files;
+      let filename = files[0].name;
+      if (filename.lastIndexOf(".") <= 0) {
+        return alert("Please add a valid field!");
+      }
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(files[0]);
+      this.image = files[0];
+      this.fileName = files[0].name;
+    },
   }
 };
 </script>
--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -4,11 +4,11 @@
       <span>Não há nenhum tópico cadastrado :(</span>
     </v-row>
     <v-row>
-      <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+      <v-col v-for="card in cards" v-bind:key="card.key" md="3">
         <v-card>
           <v-img
             max-height="300px"
-            :src="card.src"
+            :src="card.img"
             class="white--text"
             gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
           >
@@ -40,18 +40,20 @@
 import firebase from "firebase";
 export default {
   data: () => ({
-    cards: ""
+    cards: []
   }),
   mounted() {
+    firebase.database().ref("newRef/").push({'dalton': 'cesar', 'teste': 'teste'});
     var topicsRef = firebase.database().ref("topicos/");
-    topicsRef.on("value", function(snapshot) {
-      snapshot.forEach(function(child) {
+    topicsRef
+    .on("value", (snapshot) => {
+      this.cards = []
+      snapshot.forEach((child) => {
         var key = child.key;
         var childData = child.val();
-        // eslint-disable-next-line
-        console.log(key);
-        // eslint-disable-next-line
-        console.log(childData);
+        childData['key'] = key;
+        childData['img'] = "https://cdn.vuetifyjs.com/images/cards/plane.jpg";
+        this.cards.push(childData);
       });
     });
   }
